8cf70b046bf4ab9831f64b1a6a308a5f6853fa0f
moatasim-KT <moatasimfarooque@gmail.com>
2025-08-12 05:54:02 +0500

    Stop tracking files ignored by .gitignore

--- .svelte-kit/generated/client-optimized/nodes/7.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from '../../../../src/routes/knowledge/[id]/+page.svelte';
--- .svelte-kit/generated/client/nodes/9.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/relationships-demo/+page.svelte";
